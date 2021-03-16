8d82267f5b5fd6ddaeed274131e76463d13f8763
rick-masters <grick23@gmail.com>
2021-03-16 23:32:41 +0000

    Makefiles and add quotes to travis variable

--- src/c/build-all.sh
+++ src/c/build-all.sh
@@ -18,7 +18,7 @@ bazel test -c opt --copt=-DRYU_FLOAT_FULL_TABLE //ryu/...
 bazel test -c opt --copt=-DRYU_ONLY_64_BIT_OPS --copt=-DRYU_32_BIT_PLATFORM //ryu/...
 
 # osx works but installing gnuplot takes a very long time
-if [ "$TRAVIS_OS_NAME" != "windows" -a $TRAVIS_OS_NAME != "osx" ]; then
+if [ "$TRAVIS_OS_NAME" != "windows" -a "$TRAVIS_OS_NAME" != "osx" ]; then
     bazel build -c opt --jobs=1 //scripts:shortest-c-{float,double}.pdf
     bazel build -c opt --jobs=1 //scripts:{f,e}-c-double-{1,10,100,1000}.pdf
 fi
