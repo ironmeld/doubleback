a36deca9ecc60478672f496b7bd0568d43509775
rick-masters <grick23@gmail.com>
2021-03-16 23:20:57 +0000

    Disable gnuplot on osx

--- src/java/build-all.sh
+++ src/java/build-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bazel test //main/... //test/...
 bazel run //main/java/info/adams/ryu/benchmark
-if [ "$TRAVIS_OS_NAME" != "windows" ]; then
+if [ "$TRAVIS_OS_NAME" != "windows" -a $TRAVIS_OS_NAME != "osx" ]; then
     bazel build -c opt --jobs=1 //scripts:shortest-java-{float,double}.pdf
 fi
