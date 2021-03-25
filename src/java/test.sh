0504e3a5602a344f3a069b4e9da621f3a4c7467e
rick-masters <grick23@gmail.com>
2021-03-25 16:28:48 +0000

    Add diagnostics to java tests

--- src/java/test.sh
+++ src/java/test.sh
@@ -1,14 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
 
+printf "%s\n" "Starting java tests"
 bazel test //main/... //test/...
 
 set +u # allow undefined vars
 DFMT_TEST_INPUT="${DFMT_TEST_INPUT:-../test-input.csv}" 
 DFMT_TEST_EXPECTED="${DFMT_TEST_EXPECTED:-../test-expected.csv}" 
 
+printf "%s\n" "Checking for windows..."
 if [ "$TRAVIS_OS_NAME" != "windows" ]; then
+    printf "%s\n" "Running dfmt echo test..."
     set -u
     < "$DFMT_TEST_INPUT" bazel run -c opt //test/java/info/adams/ryu:dfmtecho -- > test.out
     diff "$DFMT_TEST_EXPECTED" test.out
