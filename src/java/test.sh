ba566ee1d44988c3fc36bb9d5bab4a896ece834c
rick-masters <grick23@gmail.com>
2021-03-26 16:17:56 +0000

    Change java project namespace to ironmeld

--- src/java/test.sh
+++ src/java/test.sh
@@ -8,11 +8,11 @@ if [ "$TRAVIS_OS_NAME" = "windows" ]; then
 fi
 
 printf "%s\n" "Running bazel tests..."
-bazel test //main/... //test/...
+bazel test //src/... //testsrc/...
 
 printf "%s\n" "Running dfmt echo test..."
 DFMT_TEST_INPUT="${DFMT_TEST_INPUT:-../test-input.csv}" 
 DFMT_TEST_EXPECTED="${DFMT_TEST_EXPECTED:-../test-expected.csv}" 
-< "$DFMT_TEST_INPUT" bazel run -c opt //test/java/info/adams/ryu:dfmtecho -- > test.out
+< "$DFMT_TEST_INPUT" bazel run -c opt //testsrc/com/ironmeld/doubleback:dfmtecho -- > test.out
 diff "$DFMT_TEST_EXPECTED" test.out
 rm -f test.out
