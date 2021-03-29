440b82c4ef039331d51cf4ead180cffdb4c2f6b8
rick-masters <grick23@gmail.com>
2021-03-29 16:27:03 +0000

    Improve fuzzing efficiency with vocabulary

--- src/c/fuzz.sh
+++ src/c/fuzz.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# shellcheck disable=SC2129
 
 export CC=afl-clang-fast
+WORKERS=40
 
 rm -rf fuzz
 mkdir fuzz && cd fuzz || exit 1
@@ -10,28 +12,44 @@ mkdir fuzz && cd fuzz || exit 1
 
 mkdir fuzz_in
 for i in $(seq "$(wc -l ../../test-input.csv | cut -f1 -d' ')"); do
-   echo "$i"
+   # take line # i from the input file and create a separate input file
    sed "${i}q;d"  < ../../test-input.csv > fuzz_in/"$i"
 done
+
+rm -f vocab
+for i in $(seq 0 9); do
+    printf "\"%s\"\n" "$i" >> vocab
+done
+printf "\"%s\"\n" "+" >> vocab
+printf "\"%s\"\n" "-" >> vocab
+printf "\"%s\"\n" "324" >> vocab
+printf "\"%s\"\n" "325" >> vocab
+printf "\"%s\"\n" "." >> vocab
+printf "\"%s\"\n" "e" >> vocab
+printf "\"%s\"\n" "E" >> vocab
+printf "\"%s\"\n" "000" >> vocab
+printf "\"%s\"\n" "000000" >> vocab
+printf "\"%s\"\n" "99999999999999999" >> vocab
+
 mkdir fuzz_out
 
 tmux new -d -s fuzz-master
-tmux send-keys -t fuzz-master.0 afl-fuzz SPACE -i SPACE fuzz_in SPACE -o SPACE fuzz_out SPACE -M SPACE master SPACE -- SPACE ./dfmt_echo ENTER
+tmux send-keys -t fuzz-master.0 afl-fuzz SPACE -i SPACE fuzz_in SPACE -x SPACE vocab SPACE -o SPACE fuzz_out SPACE -M SPACE master SPACE -- SPACE ./dfmt_echo ENTER
 
-for worker in $(seq 40); do
+printf "%s\n" "Fuzzing is being started. Please wait for a few seconds..."
+for worker in $(seq "$WORKERS"); do
     tmux new -d -s fuzz-worker-"$worker"
-    tmux send-keys -t fuzz-worker-"$worker".0 afl-fuzz SPACE -i SPACE fuzz_in SPACE -o SPACE fuzz_out SPACE -M SPACE worker"$worker" SPACE -- SPACE ./dfmt_echo ENTER
+    tmux send-keys -t fuzz-worker-"$worker".0 afl-fuzz SPACE -i SPACE fuzz_in SPACE -x SPACE vocab SPACE -o SPACE fuzz_out SPACE -M SPACE worker"$worker" SPACE -- SPACE ./dfmt_echo ENTER
 done
 
-printf "%s\n" "To see progress, attached to tmux window \"fuzz-master\"."
 date
-printf "%s\n" "Fuzzing for 1 hour..."
-sleep 3600
+printf "%s\n" "To see progress, attached to tmux window \"fuzz-master\"."
+printf "%s" "Press return to end fuzzing: "
+read -r _
 
-for worker in $(seq 40); do
+printf "%s\n" "Fuzzing is being terminated. Please wait for a few seconds..."
+for worker in $(seq "$WORKERS"); do
     tmux kill-window -t fuzz-worker-"$worker"
 done
 tmux kill-window -t fuzz-master
 cd ..
-ls fuzz/fuzz_out/worker*/hangs
-ls fuzz/fuzz_out/worker*/crashes
