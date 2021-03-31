09ceb379e134497216e3631f67b46d0d3af45670
rick-masters <grick23@gmail.com>
2021-03-31 15:59:53 +0000

    Debug java benchmark on travis

--- src/java/benchmark.sh
+++ src/java/benchmark.sh
@@ -4,6 +4,6 @@
 export MSYS2_ARG_CONV_EXCL="//"
 
 if [ "$TRAVIS_OS_NAME" != "windows" ] && [ "$TRAVIS_OS_NAME" != "osx" ]; then
-    bazel build -c opt --jobs=1 //scripts:java-double-shortest-64bit-data.pdf
-    bazel build -c opt --jobs=1 //scripts:java-double-shortest-bydigits-{time,length,parse}.{pdf,png}
+    bazel build -s -c opt --jobs=1 //scripts:java-double-shortest-64bit-data.pdf
+    bazel build -s -c opt --jobs=1 //scripts:java-double-shortest-bydigits-{time,length,parse}.{pdf,png}
 fi
