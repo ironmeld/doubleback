241ed3db9c73275b237d05fec0888cea4a0c2ff6
rick-masters <grick23@gmail.com>
2021-04-05 15:38:39 +0000

    Attempt java benchmarks on windows and osx

--- src/java/benchmark.sh
+++ src/java/benchmark.sh
@@ -3,7 +3,8 @@
 # makes windows work with bazel's prefix for targets
 export MSYS2_ARG_CONV_EXCL="//"
 
-if [ "$TRAVIS_OS_NAME" != "windows" ] && [ "$TRAVIS_OS_NAME" != "osx" ]; then
+# attempting benchmark on windows and osx...
+#if [ "$TRAVIS_OS_NAME" != "windows" ] && [ "$TRAVIS_OS_NAME" != "osx" ]; then
     bazel build -c opt --jobs=1 //scripts:java-double-shortest-64bit-data.pdf
     bazel build -c opt --jobs=1 //scripts:java-double-shortest-bydigits-{time,length,parse}.{pdf,png}
-fi
+#fi
