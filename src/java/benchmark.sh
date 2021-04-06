7d78bdd48378b6bcd221c0b23e6b2a9756768d3e
rick-masters <grick23@gmail.com>
2021-04-06 13:31:08 +0000

    gnuplot does not run on windows, takes too long on osx

--- src/java/benchmark.sh
+++ src/java/benchmark.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+set -e
 
 # makes windows work with bazel's prefix for targets
 export MSYS2_ARG_CONV_EXCL="//"
 
-# attempting benchmark on windows and osx...
-#if [ "$TRAVIS_OS_NAME" != "windows" ] && [ "$TRAVIS_OS_NAME" != "osx" ]; then
-    bazel build -c opt --jobs=1 //scripts:java-double-shortest-64bit-data.pdf
-    bazel build -c opt --jobs=1 //scripts:java-double-shortest-bydigits-{time,length,parse}.{pdf,png}
-#fi
+bazel build -c opt --jobs=1 //scripts:java-double-shortest-bydigits-summary.csv
+bazel build -c opt --jobs=1 //scripts:java-double-shortest-64bit-data.csv
+
+# cannot run gnuplot on windows on travis due to missing prntvpt.dll.
+if [ "$TRAVIS_OS_NAME" = "windows" ]; then
+  printf "Skipping java charts on windows. Cannot run gnuplot on windows on travis due to missing prntvpt.dll.\n"
+  exit 0
+fi
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+  # note: if you want to enable osx, add gnuplot to the homebrew package list in .travis.yml
+  printf "Skipping java charts on osx to save time.\n"
+  exit 0
+fi
+
+bazel build -c opt --jobs=1 //scripts:java-double-shortest-64bit-data.pdf
+bazel build -c opt --jobs=1 //scripts:java-double-shortest-bydigits-{time,length,parse}.{pdf,png}
